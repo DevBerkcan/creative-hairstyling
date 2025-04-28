3a9d9433bdfbb0ce472368357011244bc31a6286
Turkes, Medin <medin.turkes@realcore.de>
2025-04-28 15:58:44 +0200

    Fix pricing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import { BeforeAfter } from '../components/BeforeAfter';
 import { Footer } from '../components/Footer';
 import { Hero } from '../components/Hero';
 import { Partners } from '../components/Partners';
-import { PricingSection } from '../components/PricingSection';
 import { Services } from '../components/Services';
 import { Team } from '../components/Team';
 import { Testimonials } from '../components/Testimonials';
@@ -16,7 +15,6 @@ export const Home = () => {
       <Services />
       <Team />
       <BeforeAfter />
-      <PricingSection/>
       <Testimonials />
       <Footer />
     </div>
